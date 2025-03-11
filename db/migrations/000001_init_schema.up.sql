a9c1eed2ce5f8e032284d64762256496e7e118e9
AyushShukla09 <hbkayushshukla09@gmail.com>
2025-03-12 01:34:00 +0530

    Added database repo functions

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS "blog_posts" (
   "title" varchar NOT NULL,
   "description" varchar NOT NULL,
   "body" text NOT NULL,
-  "created_at" timestamp,
-  "updated_at" timestamp
+  "created_at" timestamp NOT NULL DEFAULT(now()),
+  "updated_at" timestamp NOT NULL DEFAULT(now())
 );
 
 COMMENT ON COLUMN "blog_posts"."body" IS 'Content of the blog post';
